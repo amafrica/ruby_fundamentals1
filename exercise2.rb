7bb3ccd5fa7cedf568808f1b12739b9cc90c4f81
Andrea <andrea.africa93@gmail.com>
2016-10-18 14:55:39 -0400

    Completed Exercise2

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+#1
+# $55 meal & 20% tip
+puts 55 * 0.20
+
+#2
+# Integer = Numbers
+# String = "whatever's inbetween these quotes"
+# Converting b/w data types: to_i - to integer
+
+puts "hello" + 5.to_s
+
+#3
+puts "The result is #{45628 * 7839}"
+
+#4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
