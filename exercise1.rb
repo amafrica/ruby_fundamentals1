7022e0bc75665acbdf47f7e65965916f5608acca
Andrea <andrea.africa93@gmail.com>
2016-10-18 14:19:19 -0400

    Completed Exercise1

---
+++ exercise1.rb
@@ -0,0 +1,17 @@
+puts 2 + 3
+
+puts "Hello World"
+
+puts "Betty's pie shop"
+
+puts 'Betty\'s pie shop'
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+puts "Hello\t\tworld"
+
+puts "Hello\b\b\b\b\bGoodbye World"
+
+puts "1. Hello\n2. World"
+
+:bitmaker
