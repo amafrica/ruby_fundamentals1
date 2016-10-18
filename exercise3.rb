4d66e53fc0429bb26f5174037774391fa95bd169
Andrea <andrea.africa93@gmail.com>
2016-10-18 15:23:10 -0400

    Prompts user for age

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,12 @@
 puts "What is your name?"
 
-name = gets
+name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "How old are you?"
+
+age = gets.chomp.to_i
+currentyear = 2016
+
+puts "You were born in #{currentyear - age}"
